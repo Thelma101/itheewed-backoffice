b28b253cfc4d550d2254402d46fcbf695c360e2a
Thelma101 <akpata.thelma@gmail.com>
2025-06-28 15:07:32 +0100

    onboarding screens refix

--- vendor-dashboard/src/pages/onboarding/OnboardingPage.tsx
+++ vendor-dashboard/src/pages/onboarding/OnboardingPage.tsx
@@ -1,9 +1,10 @@
 import NavLogo from "@/components/NavLogo";
 import type React from "react";
-import OnboardingGallery from "../../components/OnboardingGallery";
+import OnboardingGallery from "@/components/OnboardingGallery";
 import '@/index.css'
 import { useState } from "react";
 import SignIn from "@/components/SignIn";
+import SignUp from "@/components/SignUp";
 
 const images = [
     { id: 1, name: 'Venue', image: 'https://res.cloudinary.com/dfp5thr3y/image/upload/v1750539059/venue_ptzzou.svg' },
@@ -13,32 +14,29 @@ const images = [
     { id: 5, name: 'Cars', image: 'https://res.cloudinary.com/dfp5thr3y/image/upload/v1750539056/cars_wdrnq1.svg' },
 ];
 
-
 const OnboardingPage: React.FC = () => {
-
-    const [isSignUp, setIsSignUp] = useState(false);
+    const [isSignIn, setIsSignIn] = useState(true);
     return (
-        <>
-            <section className="h-screen bg-red-50">
-                <div className="container mx-auto flex flex-col">
-                    <NavLogo />
+        <section className="min-h-screen bg-[#D9D9D9] flex flex-col">
+            {/* Nav */}
+            <div className="w-full py-6 px-4 bg-[#D9D9D9]">
+                <NavLogo />
+            </div>
+            {/* Split Screen */}
+            <div className="flex flex-1 flex-col md:flex-row w-full bg-[#D9D9D9]">
+                {/* Gallery */}
+                <div className="md:w-1/2 w-full flex items-center justify-center bg-[#D9D9D9] p-4 md:p-8">
+                    <OnboardingGallery />
                 </div>
-                <div className="flex justify-center items-center h-full absolute top-10">
-                    <div>
-                        <div className="">
-                            <OnboardingGallery />
-                        </div>
-                        {isSignUp ? <SignUp /> : <SignIn /> }
-                    </div>
-                    <div className="">
-                        <button onClick={() => setIsSignUp(!isSignUp)} />
-                        <SignIn />
-                    </div>
+                {/* Form */}
+                <div className="md:w-1/2 w-full flex items-center justify-center bg-[#D9D9D9] p-4 md:p-8">
+                   {/* { isSignIn ? <SignIn /> : <SignUp /> } */}
+                   <SignUp />
                 </div>
-            </section>
-        </>
-    )
-        ;
+            </div>
+        </section>
+    );
 }
+
 
 export default OnboardingPage;
